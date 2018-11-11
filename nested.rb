837860bd21d1f380a552317727b283b75ec948f6
John Mark Redding <johnmarkredding@gmail.com>
2018-11-11 01:55:29 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -125,5 +125,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+  alans_new_info = "GUI"
+  programmer_hash[:alan_kay][:languages] << alans_new_info
+  programmer_hash
 end
